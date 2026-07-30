d5211759461255fe765a0f1c94ea4750694df6df
VIDHUBALAN <vidhubalan03@gmail.com>
2026-07-30 11:58:11 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,13 @@
+public class logical{
+	public static void main(String[]args)
+{
+	int a,b,c,d;
+	a = 10;
+	b = 20;
+	c = 40;
+	d = 35;
+        System.out.println((a<b)&&(c>d));
+	System.out.println((a>b)||(c>d));
+		
+      }
+}
